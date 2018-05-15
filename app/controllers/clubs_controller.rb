15dfc54fdb9c3b6642c5dd7a816b6de6eb899f95
borisaeric <borisa123@hotmail.com>
2018-05-15 12:57:41 +0200

    add views and controller for model Club

---
+++ app/controllers/clubs_controller.rb
@@ -0,0 +1,50 @@
+class ClubsController < ApplicationController
+  def index
+    @clubs = Club.all
+  end
+
+  def show
+    @club = Club.find(params[:id])
+  end
+
+  def new
+    @club = Club.new
+  end
+
+  def edit
+    @club = Club.find(params[:id])
+  end
+
+  def create
+    @club = Club.new(club_params)
+
+    if @club.save
+      redirect_to @club
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @club = Club.find(params[:id])
+
+    if @club.update(club_params)
+      redirect_to @club
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @club = Club.find(params[:id])
+    @club.destroy
+
+    redirect_to clubs_path
+  end
+
+  private
+  
+  def club_params
+    params.require(:club).permit(:name, :stadium)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  
+  resources :clubs
+
+  root 'welcome#index'
+
   namespace :api do
     namespace :v1 do
       resources :clubs
