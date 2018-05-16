a7d7376a2274f37c4a8acada57154dc2002651cf
borisaeric <borisa123@hotmail.com>
2018-05-16 16:20:21 +0200

    edit views for Club model and layout

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2018_05_15_042911) do
 
   create_table "seasons", force: :cascade do |t|
     t.string "name"
-    t.string "number_of_teams"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
